1d101bbda9066e83715f5b263a1ce37714815a68
Mario Guerrero Schurter <mguerreros@minpublico.cl>
2023-10-11 02:41:24 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView miListView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        agregarFAB();
+        Toast.makeText(this, getResources().getString(R.string.oncreate), Toast.LENGTH_LONG).show();
+
+        miListView = findViewById(R.id.milista);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.planetas));
+        miListView.setAdapter(arrayAdapter);
+
+        miListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(view.getContext()., R.layout.activity_detail)
+            }
+        });
+    }
+
+    public void agregarFAB(){
+        Button myButt = (Button) findViewById(R.id.button);
+        myButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getBaseContext(), "asd", Toast.LENGTH_SHORT).show();
+                Snackbar.make(view, "soy snack", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, getResources().getString(R.string.onstart), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, getResources().getString(R.string.onresume), Toast.LENGTH_LONG).show();
+    }
+    // corriendo
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, getResources().getString(R.string.onrestart), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, getResources().getString(R.string.onpause), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, getResources().getString(R.string.onstop), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, getResources().getString(R.string.ondestroy), Toast.LENGTH_LONG).show();
+    }
+}
